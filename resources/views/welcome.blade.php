d251138056b628b0fd848e757968fb1adee7d552
jpmendoza <jpmendoza@p3plcpnl1014.prod.phx3.secureserver.net>
2015-05-12 22:12:08 -0700

    First push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>Jorge P Mendoza</title>
+		
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #0f0ca8;
+				display: table;
+				font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				margin-bottom: 40px;
+			}
+
+			.quote {
+				font-size: 24px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">Jorge P. Mendoza</div>
+				<div class="quote">Spending more time on my website</div>
+			</div>
+		</div>
+	</body>
+</html>
