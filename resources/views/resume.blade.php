1cb6d05e22e141a6da3e7ad88abc095aa9bf7710
Jorge Mendoza <jorge.mendoza415@gmail.com>
2015-05-26 17:37:00 -0700

    starting resume page

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.default')
+
+@section('title')
+{{ $title }}
+@stop
+
+@section('content')
+<div class="container">
+	<div align="center"> <h1> RESUME </h1></div>
+	<div >&nbsp;</div>
+	<div >&nbsp;</div>
+</div>
+
+
+@stop
