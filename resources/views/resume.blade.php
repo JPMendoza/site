17c873575108006c503b2f33ffa787144a6dbb0b
Jorge Mendoza <jorge.mendoza415@gmail.com>
2015-05-26 18:58:46 -0700

    resume mid-update

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -6,9 +6,80 @@
 
 @section('content')
 <div class="container">
-	<div align="center"> <h1> RESUME </h1></div>
-	<div >&nbsp;</div>
-	<div >&nbsp;</div>
+	<div align="center"> <h1> Jorge Peradilla Mendoza </h1></div>
+	<div class="row">
+		<div class="col-md-6">
+			San Francisco, California
+		</div>
+		<div align="right" class="col-md-6">
+			https://github.com/JPMendoza
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			jorge.mendoza415@gmail.com
+		</div>
+		<div align="right" class="col-md-6">
+			https://linkedin.com/in/jorgepmendoza
+		</div>
+	</div>
+	<div style="background-color: #D3D3D3;" class="row">
+		<div class="col-md-6">
+			<b>Objective</b>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			To grow as a programmer by learning as many new types of skills sets related to the Computer Science field to be a well rounded 
+			Software Engineer
+		</div>
+	</div>
+	<div class="row">
+	</div>
+	<div style="background-color: #D3D3D3;" class="row">
+		<div class="col-md-6">
+			<b>Skills</b>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			Java, JavaScript, C, C++, C#, HTML, SQL, JDBC, PHP, CSS, Python, Ruby, jQuery, Vim, Eclipse, Microsoft
+			Visual Studio, Bash, Yii, REST, Junit, XML, CSV, LATEX, SVN, Git, Mercurial, UNIX, Linux, Jira, Agile
+			Development
+		</div>
+	</div>
+	<div style="background-color: #D3D3D3;" class="row">
+		<div class="col-md-6">
+			<b>Work Experience</b>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			
+		</div>
+	</div>
+	<div style="background-color: #D3D3D3;" class="row">
+		<div class="col-md-6">
+			<b>Projects</b>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			See <a href="/projects">project page</a> 
+		</div>
+	</div>
+	<div style="background-color: #D3D3D3;" class="row">
+		<div class="col-md-6">
+			<b>Education</b>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			Java, JavaScript, C, C++, C#, HTML, SQL, JDBC, PHP, CSS, Python, Ruby, jQuery, Vim, Eclipse, Microsoft
+			Visual Studio, Bash, Yii, REST, Junit, XML, CSV, LATEX, SVN, Git, Mercurial, UNIX, Linux, Jira, Agile
+			Development
+		</div>
+	</div>
 </div>
 
 
