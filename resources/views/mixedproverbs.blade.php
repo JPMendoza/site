a4fc91cd3f3aef5c20b3ab001c9e5090f105de47
Jorge Mendoza <jorge.mendoza415@gmail.com>
2015-06-03 00:58:31 -0700

    added php page that goes along with mixed proverbs

---
+++ resources/views/mixedproverbs.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.default')
+
+@section('title')
+{{ $title }}
+@stop
+
+@section('content')
+<div align="center"> <h1> Mixed Proverbs </h1></div>
+<div id="mixed" align="center"> 
+
+ </div>
+ <div align="center">
+ 	<input id="clickMe" type="button" value="clickme" onclick="getProverb();" >
+</div>
+
+
+<script>
+function getProverb() {
+	 $.get("/api/v1/proverb", function (data) {
+		$("#mixed").html(data);
+	});
+}
+</script>
+
+@stop
