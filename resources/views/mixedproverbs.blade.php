67a5fd5e78c3871b0f5fd39b2b1a406705db52e7
Jorge Mendoza <jorge.mendoza415@gmail.com>
2015-06-03 02:00:09 -0700

    fixed typo

--- resources/views/mixedproverbs.blade.php
+++ resources/views/mixedproverbs.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div align="center"> <h1> Mixed Proverbs </h1></div>
 <div id="mixed" align="center"> 
-Click the button to for new proverbs!
+Click the button for new proverbs!
  </div>
  <div>&nbsp;</div>
  <div align="center">
