5c0dc98c2026e9fcff8e8166ea50b4f3416603b4
Jorge Mendoza <jorge.mendoza415@gmail.com>
2015-07-08 00:54:10 -0700

    update links

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -31,18 +31,18 @@
 	</div>
 	<div class="row">
 		<div align="center" class=" container col-md-4">
-			<a href="{{ asset('/site/public/projects/alienabduction/alienabduction.html') }}" >Alien Abduction<br>
+			<a href="{{ asset('/site/public/games/alienabduction/alienabduction.html') }}" >Alien Abduction<br>
 				<img src="{{asset('/site/public/img/alien.png')}}" height="150" width="200"></a> <br>
 				*Chrome doesn't support unity 
 			
 		</div>
 		<div align="center" class="container col-md-4">
-			<a href="{{ asset('/site/public/projects/heart4u/heart4u.html') }}"> Heart4u <br> 
+			<a href="{{ asset('/site/public/games/heart4u/heart4u.html') }}"> Heart4u <br> 
 				<img src="{{asset('/site/public/img/heart4u.png')}}" height="150" width="200">
 			</a>
 		</div>
 		<div align="center" class="container col-md-4">
-			<a href="{{ asset('/site/public/projects/cubeclicker/cubeclicker.html') }}" >CubeClicker <br> 
+			<a href="{{ asset('/site/public/games/cubeclicker/cubeclicker.html') }}" >CubeClicker <br> 
 				<img src="{{asset('/site/public/img/cube.png')}}" height="150" width="200">
 			</a>
 		</div>
@@ -55,7 +55,7 @@
 	<div align="center" class=" container col-md-4">
 			<a href="/MixedSayings" >Mixed Sayings<br>
 				<img src="{{asset('/site/public/img/mixedsaying.png')}}" height="150" width="200"></a> <br>
-				*Chrome doesn't support unity 
+			
 			
 		</div>
 
